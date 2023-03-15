978438df3a2f72d963f5b212fe749dc61790bb9c
Carlos Pecina <jukarpenz@gmail.com>
2023-03-14 18:53:43 -0600

    First commit

---
+++ src/main/java/com/jukarpenz/cert/prep/ocpjs17d/c1/C1_BuildingBlocks.java
@@ -0,0 +1,24 @@
+package com.jukarpenz.cert.prep.ocpjs17d.c1;
+
+public class C1_BuildingBlocks {
+
+
+
+}
+
+/**
+ * This element is a class
+ */
+class Animal{
+
+    String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
